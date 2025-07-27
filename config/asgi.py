9dc4cf36bf7767988b375e914e096c8d02ec87c8
Himanshu Lodhi <himanshulodhi17@gmail.com>
2025-07-27 20:52:56 +0530

    renamed some folder and files

--- config/asgi.py
+++ config/asgi.py
@@ -9,15 +9,15 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
-import thinkflow.routing
+import config.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thinkflow.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
-            URLRouter(thinkflow.routing.websocket_urlpatterns)
+            URLRouter(config.routing.websocket_urlpatterns)
         )
     ),
 })
--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import landingpage
 
 urlpatterns = [
+    path('', landingpage),
     path('admin/', admin.site.urls),
     path('api/', include('documents.urls')),
     path('api/auth/', include('users.urls')),
