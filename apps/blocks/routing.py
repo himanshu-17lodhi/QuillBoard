973d285b6bb0d731d926a38bf160904704070b16
Himanshu Lodhi <125793095+himanshu-17lodhi@users.noreply.github.com>
2025-08-01 15:26:10 +0530

    Delete apps directory

--- apps/blocks/routing.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(
-        r'ws/collaboration/(?P<page_id>[^/]+)/$',
-        consumers.CollaborationConsumer.as_asgi()
-    ),
-]
--- apps/workspaces/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class WorkspacesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.workspaces'
-    label = 'quillboard_workspaces'
-
-    def ready(self):
-        import apps.workspaces.signals
